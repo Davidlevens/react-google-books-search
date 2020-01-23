d036caa4c7de74da0f4fed96eb0c5b29dfe578fa
David Levens <52017470+Davidlevens@users.noreply.github.com>
2020-01-23 15:31:28 -0500

    Added get and put to api/books /:id route

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
-// same as "/api/books"
-router.route("/")
+// Same as "/api/books"
+router
+  .route("/")
   .get(booksController.findAll)
   .post(booksController.create);
 
-// same as "/api/books/:id"
+// Same as "/api/books/:id"
 router
   .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
   .delete(booksController.remove);
 
 module.exports = router;
