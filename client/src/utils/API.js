d1c7690a303f5d7690f0b1bac6fd407d1f0a6fe7
David Levens <52017470+Davidlevens@users.noreply.github.com>
2020-01-24 16:26:03 -0500

    Minor path corrections to books and index api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,22 +1,17 @@
 import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 export default {
-  // Gets books from the Google API
-  getBooks: function(q) {
-    return axios.get("/api/google", {
-      params: { q: "title:" + q }
-    });
+  searchBooks: function(query) {
+    return axios.get(BASEURL + query);
   },
-  // Gets all saved books
-  getSavedBooks: function() {
-    return axios.get("/api//books");
+  getBooks: function() {
+    return axios("/api/books");
   },
-  // Deletes the saved book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api//books/" + id);
-  },
-  // Saves an book to the database
   saveBook: function(bookData) {
-    return axios.post("/api//books", bookData);
+    return axios.post("/api/books", bookData);
+  },
+  deleteBook: function(id) {
+    return axios.delete("api/books/" + id)
   }
 };
--- routes/api/books.js
+++ routes/api/books.js
@@ -2,16 +2,15 @@ const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
 // Same as "/api/books"
-router
-  .route("/")
+router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
 
 // Same as "/api/books/:id"
 router
   .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
+  // .get(booksController.findById)
+  // .put(booksController.update)
   .delete(booksController.remove);
 
 module.exports = router;
